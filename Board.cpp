06349b88f1485079c4183bc1934f84a3f1809b28
Anthony Wu <awu42@jhu.edu>
2018-04-26 13:31:32 -0400

    Edited Board.cpp a ton

--- Board.cpp
+++ Board.cpp
@@ -8,46 +8,81 @@
 #include "CreatePiece.h"
 
 
-/////////////////////////////////////
 // DO NOT MODIFY THIS FUNCTION!!!! //
-/////////////////////////////////////
 Board::Board( void ){}
 
 const Piece* Board::operator()( std::pair< char , char > position ) const
-{
-	/////////////////////////
-	// [REPLACE THIS STUB] //
-	/////////////////////////
+{		
+	if(_occ.find(position) != _occ.end()) {
+		// This position is occupied by a piece
+		Piece* my_pointer = &position;
+		return my_pointer;
+	}
 	return NULL;
 }
 
 bool Board::add_piece( std::pair< char , char > position , char piece_designator )
 {
-	/////////////////////////
-	// [REPLACE THIS STUB] //
-	/////////////////////////
+	
+	if(_occ.find(position) != _occ.end()) {
+		// Piece already exists
+		return false;
+	}
+	if(position.first < 'A' || position.first > 'H') {
+		// Wrong Letter
+		return false;
+	}
+	if(position.second < 1 || position.second > 8) {
+		// Wrong Number
+		return false;
+	}
+	vector<char> pieces = {'k', 'K', 'q', 'Q', 'b', 'B', 'N', 'n', 'R',
+		'r', 'p', 'P'};
+	vector<char>::iterator it;
+	it = find(pieces.begin(), pieces.end(), piece_designator);
+	if(it != pieces.end()) {
+		// Piece designator is found
+	}
+	else {
+		// Piece designator not found
+		return false;
+	}
 	_occ[ position ] = create_piece( piece_designator );
 	return true;
 }
 
+// Loops through map to see if there's either 'K' or 'k'
+// If there's one each, return true, else return false
 bool Board::has_valid_kings( void ) const
 {
-	/////////////////////////
-	// [REPLACE THIS STUB] //
-	/////////////////////////
-	return true;
+	int black_king = 0;
+	int white_king = 0;
+	for(map<pair<char, char>, char>::iterator it = _occ.begin();
+			it != _occ.end(); ++it) {
+		if(it->second == 'K') {
+			white_king = white_king + 1;
+		}
+		else if(it->second == 'k') {
+			black_king = black_king + 1;
+		}
+	}
+	if(black_king == 1 && white_king == 1) {
+		return true;
+	}
+	else {
+		return false;
+
+	}
 }
 
 void Board::display( void ) const
 {
-	/////////////////////////
-	// [REPLACE THIS STUB] //
-	/////////////////////////
+	// Display the board
+	cout << *this << endl;
+	
 }
 
-/////////////////////////////////////
 // DO NOT MODIFY THIS FUNCTION!!!! //
-/////////////////////////////////////
 std::ostream& operator << ( std::ostream& os , const Board& board )
 {
 	for( char r='8' ; r>='1' ; r-- )
