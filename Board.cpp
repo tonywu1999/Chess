d9e2c340935ceb81ac98fd0820647f027d3a57c2
Anthony Wu <awu42@jhu.edu>
2018-05-02 21:21:00 -0400

    valgrind loading wack

--- Board.cpp
+++ Board.cpp
@@ -278,11 +278,18 @@ void Board::reverse_execute(pair<char, char> start, pair<char, char> end, Piece*
 }
 
 void Board::clear_board() {
+	/*
 	for(map<pair<char, char>, Piece*>::const_iterator it = _occ.cbegin(); it != _occ.cend(); ++it) {
                 Piece* deleted = it->second;
 		_occ.erase(it->first);
-		delete deleted;
+		if (deleted != NULL) {
+			delete deleted;
+		}
+	
         }
+	*/
+
+	_occ.clear();
 }
 
 // Loops through map to see if there's either 'K' or 'k'
